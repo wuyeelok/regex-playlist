98af6f5c400db5ad2ce8d045ce676a5d94caefb7
wuyeelok <wuyeelok@gmail.com>
2019-01-21 11:52:20 +0800

    My solution on checking username

--- validation.js
+++ validation.js
@@ -1,1 +1,15 @@
 // validation script here
+const usernameRegex = new RegExp(/^[\w]{5,12}$/)
+
+const usernameEle = document.getElementsByName("username")[0]
+
+if(usernameEle !== undefined && usernameEle !== null) {
+    usernameEle.oninput = () => {
+        const userInput = usernameEle.value
+        if(usernameRegex.test(userInput)) {
+            usernameEle.style.borderColor = "green"
+        } else {
+            usernameEle.style.borderColor = "red"
+        }
+    }
+}
