33b5a4a1d92b7d828b2239f94618c1d77cf42cd6
wuyeelok <wuyeelok@gmail.com>
2019-01-21 15:44:51 +0800

    Refactor: Use validate function to set borderColor

--- validation.js
+++ validation.js
@@ -1,4 +1,27 @@
 // validation script here
+const inputs = document.querySelectorAll("input")
+
+const patterns = {
+    username: /^[\w]{5,12}$/,
+    email: /^[\w]+@[\w]+.com$/i,
+    password: /^[a-z,0-9,@,_,-]{8,20}$/i,
+    telephone: /^[\d]{11}$/,
+    slug: /^[a-z,0-9,-]{8,20}$/
+}
+
+//  validation function
+const validate = (field, regex) => {
+    return regex.test(field.value)
+}
+
+inputs.forEach(input => {
+    input.addEventListener('input', e => {
+        const borderColor = (validate(e.target, patterns[e.target.attributes.name.value])?"green":"red")
+        e.target.style.borderColor = borderColor
+    })
+})
+
+/*
 const setInputFieldBorderColor = (inputFieldEle, regex) => {
     if(inputFieldEle !== undefined && inputFieldEle !== null) {
         inputFieldEle.oninput = () => {
@@ -24,4 +47,5 @@ setInputFieldBorderColor(usernameEle, usernameRegex)
 //  telephone
 const telephoneEle = document.getElementsByName("telephone")[0]
 const telephoneRegex = /^[\d]{11}$/
-setInputFieldBorderColor(telephoneEle, telephoneRegex)
+setInputFieldBorderColor(telephoneEle, patterns.telephone)
+*/
