6e72cfce2e5cd0221969fb0fd52e2f6511eab9a5
wuyeelok <wuyeelok@gmail.com>
2019-01-21 16:55:58 +0800

    Fix pattern for email

--- validation.js
+++ validation.js
@@ -3,7 +3,7 @@ const inputs = document.querySelectorAll("input")
 
 const patterns = {
     username: /^[a-z\d]{5,12}$/i,
-    email: /^[\w]+@[\w]+.com$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i,
     password: /^[\w@-]{8,20}$/i,
     telephone: /^[\d]{11}$/,
     slug: /^[a-z\d-]{8,20}$/
