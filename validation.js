553326ac284cd7174d3d25bcf814dffd061f66c1
wuyeelok <wuyeelok@gmail.com>
2019-01-21 12:18:51 +0800

    Refactor: Extract change border color function

--- validation.js
+++ validation.js
@@ -1,15 +1,21 @@
 // validation script here
-const usernameRegex = new RegExp(/^[\w]{5,12}$/)
-
-const usernameEle = document.getElementsByName("username")[0]
-
-if(usernameEle !== undefined && usernameEle !== null) {
-    usernameEle.oninput = () => {
-        const userInput = usernameEle.value
-        if(usernameRegex.test(userInput)) {
-            usernameEle.style.borderColor = "green"
-        } else {
-            usernameEle.style.borderColor = "red"
+const setInputFieldBorderColor = (inputFieldEle, regex) => {
+    if(inputFieldEle !== undefined && inputFieldEle !== null) {
+        inputFieldEle.oninput = () => {
+            const userInputValue = inputFieldEle.value
+            if(userInputValue !== undefined && userInputValue !== null) {
+                if(regex.test(userInputValue)) {
+                    inputFieldEle.style.borderColor = "green"
+                } else {
+                    inputFieldEle.style.borderColor = "red"
+                }
+            }
         }
     }
 }
+
+
+// username
+const usernameEle = document.getElementsByName("username")[0]
+const usernameRegex = /^[\w]{5,12}$/i
+setInputFieldBorderColor(usernameEle, usernameRegex)
