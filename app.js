db04ad709d32097094757888bda6c90222eacc0f
wuyeelok <wuyeelok@gmail.com>
2019-01-23 16:21:31 +0800

    Add bracket

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const vaxhackerPatterns = {
-    dot: /XY..Y/
+    dot: /XY..Y/,
+    bracket: /[ABC][AT]T/
 }
 
+/*
 console.log(vaxhackerPatterns.dot.test("XY1Y"))
 console.log(vaxhackerPatterns.dot.test("XY12Y"))
 console.log(vaxhackerPatterns.dot.test("XY#$Y"))
 console.log(vaxhackerPatterns.dot.test("XY Y"))
 console.log(vaxhackerPatterns.dot.test("XY  Y"))
+*/
+
+console.log(vaxhackerPatterns.bracket.test("CAT"))
+console.log(vaxhackerPatterns.bracket.test("BBT"))
